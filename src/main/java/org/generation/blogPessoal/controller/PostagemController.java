96bdadcbf598c70b7de9e27dab9184d324fb16ca
joaozinsh <joaogabriel241201@gmail.com>
2021-05-27 20:34:58 -0300

    feat. adicionar, atualizar e remover postagens

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -7,8 +7,12 @@ import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +50,20 @@ public class PostagemController {
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}	
+	
+	@PostMapping
+	public ResponseEntity<Postagem> savePostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(201).body(repository.save(postagem));
 	}
 	
+	@PutMapping
+	public ResponseEntity<Postagem> updatePostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(200).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
