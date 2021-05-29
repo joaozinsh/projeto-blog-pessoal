d6241372f0ce7f0ad6bdc04d504f4efda338d5f4
joaozinsh <joaogabriel241201@gmail.com>
2021-05-28 22:38:19 -0300

    feat: adicionando tema para as postagens e melhoria nas postagens

---
+++ src/main/java/org/generation/blogPessoal/service/TemaService.java
@@ -0,0 +1,25 @@
+package org.generation.blogPessoal.service;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Tema;
+import org.generation.blogPessoal.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemaService {
+
+	@Autowired
+	private TemaRepository repository;
+
+	public Optional<Tema> cadastrarTema(Tema novoTema) {
+		Optional<Tema> temaExistente = repository.findByDescricaoContainingIgnoreCase(novoTema.getDescricao());
+		if (temaExistente.isPresent()) {
+			return Optional.empty();
+		} else {
+			return Optional.of(repository.save(novoTema));
+		}
+	}
+
+}
