b1818e3ed83ae620a2ff373f6475dae0f98feeaa
joaozinsh <joaogabriel241201@gmail.com>
2021-06-07 19:13:36 -0300

    feature: implementando camadas de segurança

---
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,35 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.logar(user).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElse(ResponseEntity.status(401).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(201).body(usuarioService.cadastrarUsuario(usuario));
+	}
+
+}
